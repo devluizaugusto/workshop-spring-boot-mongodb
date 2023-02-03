850e7305d26dc894b2267fcbd146665b2b8f0ea3
Luiz Augusto <dev.luiz.augusto@gmail.com>
2023-02-03 14:00:40 -0300

    Insertion of a user with POST

--- src/main/java/com/luizaugusto/worshopmongo/services/UserService.java
+++ src/main/java/com/luizaugusto/worshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.luizaugusto.worshopmongo.domain.User;
+import com.luizaugusto.worshopmongo.dto.UserDTO;
 import com.luizaugusto.worshopmongo.repository.UserRepository;
 import com.luizaugusto.worshopmongo.services.exception.ObjectNotFoundException;
 
@@ -23,5 +24,13 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> user = repo.findById(id);
 		return user.orElseThrow(() -> new ObjectNotFoundException("USER NOT FOUND"));
+	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 }
