e011c57b327925a1a1f1915741261626d891d1a4
Luiz Augusto <dev.luiz.augusto@gmail.com>
2023-02-03 13:40:54 -0300

    Getting user by id

--- src/main/java/com/luizaugusto/worshopmongo/services/UserService.java
+++ src/main/java/com/luizaugusto/worshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.luizaugusto.worshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.luizaugusto.worshopmongo.domain.User;
 import com.luizaugusto.worshopmongo.repository.UserRepository;
+import com.luizaugusto.worshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,10 @@ public class UserService {
 	
 	public List<User> findAll(){
 		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> user = repo.findById(id);
+		return user.orElseThrow(() -> new ObjectNotFoundException("USER NOT FOUND"));
 	}
 }
