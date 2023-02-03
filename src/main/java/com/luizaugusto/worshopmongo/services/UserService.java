93c4ab6c71a96c0f3c664ff3207fd7f4d7ee9f22
Luiz Augusto <dev.luiz.augusto@gmail.com>
2023-02-03 14:25:30 -0300

    User delete with DELETE

--- src/main/java/com/luizaugusto/worshopmongo/services/UserService.java
+++ src/main/java/com/luizaugusto/worshopmongo/services/UserService.java
@@ -16,20 +16,25 @@ public class UserService {
 
 	@Autowired
 	private UserRepository repo;
-	
-	public List<User> findAll(){
+
+	public List<User> findAll() {
 		return repo.findAll();
 	}
-	
+
 	public User findById(String id) {
 		Optional<User> user = repo.findById(id);
 		return user.orElseThrow(() -> new ObjectNotFoundException("USER NOT FOUND"));
 	}
-	
+
 	public User insert(User obj) {
 		return repo.insert(obj);
 	}
-	
+
+	public void delete(String id) {
+		findById(id);
+		repo.deleteById(id);
+	}
+
 	public User fromDTO(UserDTO objDto) {
 		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
